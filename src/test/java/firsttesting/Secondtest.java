57ee031a2aa6f26e274d6538feb9c51b9ecb61da
bapparaya <bryan.w@kanrad.com>
2025-05-05 11:05:40 +0530

    second page

---
+++ src/test/java/firsttesting/Secondtest.java
@@ -0,0 +1,17 @@
+package firsttesting;
+
+import org.testng.annotations.Test;
+
+public class Secondtest {
+
+	
+	@Test
+	void login() {
+		System.out.println("login test");
+	}
+	
+	@Test
+	void homepage() {
+		System.out.println("it is home page");
+	}
+}
